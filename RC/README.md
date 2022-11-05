edc009049431b280f3b2817d1240b551a6d611fb
Mint1832 <mint18032@gmail.com>
2022-11-05 22:07:33 +0700

    Add RC with an error.

---
+++ RC/README.md
@@ -0,0 +1,14 @@
+# DEMO SELENIUM REMOTE CONTROL
+
+- Chạy [selenium RC](https://www.selenium.dev/documentation/legacy/selenium_1/) test sử dụng python
+
+## Chuẩn bị
+- Phần mềm [Visual studio code](https://code.visualstudio.com/download)
+- [Python & Pip](https://www.python.org/downloads/)
+- [Selenium Server](../selenium-server-standalone-3.12.0.jar)
+- Thư viện Selenium `pip install selenium`
+- `pip install html-testRunner`
+
+## Chạy test
+- Mỗi khi chạy, cần chạy Selenium Server (mặc định ở LocalHost:4444): `java -jar selenium-server-standalone-3.12.0.jar`
+- Ở một terminal khác, chạy file [main.py](./main.py): `py main.py`
---
+++ RC/tests/test.py
@@ -0,0 +1,26 @@
+import selenium
+import HtmlTestRunner
+import unittest, time, re
+
+class NewTest(unittest.TestCase):
+    def setUp(self):
+        print("========== [Begin Test] ==========")
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "http://www.google.com/")
+        self.selenium.start()
+    
+    def test_new(self):
+        sel = self.selenium
+        sel.open("/")
+        sel.type("q", "selenium rc")
+        sel.click("btnG")
+        sel.wait_for_page_to_load("30000")
+        self.failUnless(sel.is_text_present("Results * for selenium rc"))
+    
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+        print("========== [ End Test ] ========== \n")
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='../reports'))
