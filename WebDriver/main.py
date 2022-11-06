666d3c7252f40ecf0fd882f860eb0da365f70747
johnhoangsoftware <johnhoangsoftware@gmail.com>
2022-11-07 01:35:38 +0700

    feat: clean code

--- WebDriver/main.py
+++ WebDriver/main.py
@@ -1,41 +1,14 @@
-from tests.login_test import MyTestCase
 
 
-# def main():
-#     service = Service(executable_path=ChromeDriverManager().install())
-#     options = Options()
-#     options.add_argument("--disable-notifications")
-#     # Step 1) 
-#     browser = webdriver.Chrome(service=service, options=options)
-#     # Step 2) Navigate to Facebook
-#     browser.get("http://www.facebook.com")
 
-#     # Step 3) Search & Enter the Email or Phone field & Enter Password
-#     username = browser.find_element(By.ID,"email")
-#     time.sleep(1)
-#     username.send_keys("0904838631")
-#     time.sleep(1)
-
-#     password = browser.find_element(By.ID,"pass")
-#     password.send_keys("Johnhoang2510")
-#     time.sleep(1)
-
-#     # Step 4) Click Login
-#     password.send_keys(Keys.ENTER)
-#     time.sleep(10)
-    
-    
-
-
-#     menu = browser.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]")
-#     menu.click()
-#     time.sleep(3)
-#     time.sleep(3)
-#     browser.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div/div[5]/div").click()
-#     time.sleep(3)
-
-#     browser.close()
+def main():
+    # browser = custom_chrome()
+    # browser.get("http://www.facebook.com")
+    data_test = open("./tests/data_test", 'r').read().split('\n')
+    print("a")
 
 
 if __name__ == '__main__':
-    MyTestCase()
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- WebDriver/pages/home_page.py
+++
@@ -1,5 +0,0 @@
-def get_xpath_button_profile():
-    return "/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]"
-
-def get_xpath_button_logout():
-    return "/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[2]/div/div[5]/div"
